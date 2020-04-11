29b3504cb13cdc301d78770ebe465980320fc7b8
deathnote61 <61727783+deathnote61@users.noreply.github.com>
2020-04-11 20:26:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
     ipcMain
 } = electron;
 
-let todayWindow, createWindow, listWindows;
+let todayWindow, createWindow, listWindow;
 
 app.on('ready', ()=> {
     todayWindow = new BrowserWindow({
@@ -22,4 +22,82 @@ app.on('ready', ()=> {
         app.quit();
         todayWindow = null;
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null))
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null))
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+	console.log(appointment);
+});
+ipcMain.on("appointment:request:list", event => {
+	console.log("here");
+});
+ipcMain.on("appointment:request:today", event => {
+	console.log("here2");
+});
+ipcMain.on("appointment:done", (event, id) => {
+	console.log("here3");
+});
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "New Appointment",
+
+        click(){
+            createWindowCreator();
+        }
+    },
+    {
+        label: "All Appointments",
+        click(){
+            listWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        accelerator:process.platform == "darwin" ? "command+Q" : "Ctrl + Q",
+        click(){
+            app.quit();
+        }
+    }
+]
+},
+
+{
+    label: "View",
+    submenu: [{ role:"reload" }, { role : "toggledevtools"}]
+}
+]
