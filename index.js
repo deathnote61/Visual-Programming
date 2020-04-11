f3df055a753d64c8dca91df84afadab2fa22f6a7
deathnote61 <61727783+deathnote61@users.noreply.github.com>
2020-04-11 22:40:33 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require("electron");
-
+const uuid = require("uuid").v4;
 const {
     app,
     BrowserWindow, 
@@ -7,7 +7,11 @@ const {
     ipcMain
 } = electron;
 
-let todayWindow, createWindow, listWindows;
+let todayWindow;
+let createWindow;
+let listWindow;
+
+let allAppointments = [];
 
 app.on('ready', ()=> {
     todayWindow = new BrowserWindow({
@@ -22,4 +26,88 @@ app.on('ready', ()=> {
         app.quit();
         todayWindow = null;
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null))
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null))
+};
+
+ipcMain.on("appointment:create", (event, appointment) => {
+    appointment["id"] = uuid();
+    appointment["done"] = 0;
+    allAppointments.push(appointment);
+    
+    createWindow.close();
+
+	console.log(allAppointments);
+});
+ipcMain.on("appointment:request:list", event => {
+	listWindow.webContents.send('appointment:response:list', allAppointments);
+});
+ipcMain.on("appointment:request:today", event => {
+	console.log("here2");
+});
+ipcMain.on("appointment:done", (event, id) => {
+	console.log("here3");
+});
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "New Appointment",
+
+        click(){
+            createWindowCreator();
+        }
+    },
+    {
+        label: "All Appointments",
+        click(){
+            listWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        accelerator:process.platform == "darwin" ? "command+Q" : "Ctrl + Q",
+        click(){
+            app.quit();
+        }
+    }
+]
+},
+
+{
+    label: "View",
+    submenu: [{ role:"reload" }, { role : "toggledevtools"}]
+}
+]
